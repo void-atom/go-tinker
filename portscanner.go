41d7d50b785aab7b8970ab3407339fc262f6fb0c
Ishwor Basyal <ishwor8basyal@gmail.com>
2025-04-12 18:21:48 +0545

    Streamlined?

--- portscanner.go
+++ portscanner.go
@@ -6,6 +6,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -13,16 +14,21 @@ func portScanner(host string, port string, timeout time.Duration) {
 	address := host + ":" + port
 	fmt.Println(address)
 
-	// Simple TCP connection to test if the port is listening
 	conn, err := net.DialTimeout("tcp", address, timeout)
-	if err != nil {
-		fmt.Printf("Port %s is not available: %v\n", port, err)
-		return
+	if err == nil {
+		fmt.Printf("Success: Port %s is open on %s\n", port, host)
+		conn.Close()
+	} else {
+		if strings.Contains(err.Error(), "too many open files") {
+			fmt.Println("Server might be busy. Might have to handle it later")
+			return
+		} else {
+			fmt.Printf("Failed: Port %s is closed on %s\n", port, host)
+		}
 	}
-	defer conn.Close()
 
-	fmt.Printf("Port %s is open and available!\n", port)
 }
+
 func main() {
 	// Google DNS
 	portScanner("8.8.8.8", "53", 2*time.Second)
